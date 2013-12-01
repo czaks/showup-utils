ad5c90d12e39df61259f771b82ed0542c5ac6b0c
Marcin Łabanowski <marcin@6irc.net>
2013-12-01 03:10:58 +0100

    document scripts a bit, fix permissions

--- suurl.sh
+++ suurl.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Usage: ./suurl.sh nickname
+# ---------------------------
+# Returns rtmp url of selected transmission
 DATA=`curl -b "accept_rules=true" "http://showup.tv/$1" 2>/dev/null \
   | egrep 'startChildBug|srvE' \
   | ruby -e "puts gets.split(/'/)[3]; puts gets.split(/'/)[1]"`
