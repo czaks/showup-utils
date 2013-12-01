3dd2bbfd0bf2e1ad9773786f00f3af177ddcca91
Marcin Łabanowski <marcin@6irc.net>
2013-12-01 03:19:44 +0100

    add missing file from previous commit

--- sudump.sh
+++ sudump.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Usage: ./sudump nickname > dump.flv
+# Usage: ./sudump.sh nickname > dump.flv
 # -----------------------------------
 # Dumps selected transmission to stdout
 rtmpdump -r `./suurl.sh "$1"` -b 0
